b7d7b3f1cc292306262690f8eabfd767d0ce8e2a
Ramyabharathi <75662241+ramyabharathi-02@users.noreply.github.com>
2020-12-08 13:34:16 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,20 @@
+#(1)
+def value(a, b):
+
+ print ("the addition of numbers",a+b)
+ print("the subraction of numbers",a-b)
+ print("the division of number",a/b)
+ print("the multiplication of the number",a*b)
+a = int(input("enter the number:"))
+b = int(input("enter the number"))
+value(a,b)
+
+
+
+#(2)
+def covid(self):
+    s = input("enter the name:")
+    bt = float(input("enter the body temperature:"))
+    bt = 98
+    print("name",s)
+    print("bodu temperature:",bt)
